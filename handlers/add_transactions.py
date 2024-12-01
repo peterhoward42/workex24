2bba9b505bcbd979664d560a903036426db43632
peterhoward42 <peterhoward42@gmail.com>
2024-12-01 22:29:32 +0000

    try autoformat on one file

--- handlers/add_transactions.py
+++ handlers/add_transactions.py
@@ -1,23 +1,26 @@
-
 from typing import List
 from fastapi import HTTPException
 from models.transaction import Transaction
 from storage.in_memory_store import store_transactions
 
+
 def add_transactions(csv_data: bytes) -> List[Transaction]:
     transactions = build_transactions(csv_data)
-    store_transactions(transactions) 
+    store_transactions(transactions)
     return transactions
 
+
 def build_transactions(csv_data: bytes) -> List[Transaction]:
-    
+
     try:
-        s = csv_data.decode('UTF-8')
+        s = csv_data.decode("UTF-8")
     except Exception as err:
-        raise HTTPException(status_code=400, detail=f'Cannot decode your CSV input. Details: {str(err)}')
-    
+        raise HTTPException(
+            status_code=400, detail=f"Cannot decode your CSV input. Details: {str(err)}"
+        )
+
     lines = s.splitlines()
-    
+
     transactions = []
     for line in lines:
         if len(line) == 0:
@@ -26,21 +29,27 @@ def build_transactions(csv_data: bytes) -> List[Transaction]:
             continue
         if line[0] == "#":
             continue
-        
+
         transaction = parse_transaction(line)
         transactions.append(transaction)
-           
+
     return transactions
+
 
 def parse_transaction(line: str) -> Transaction:
     fields = [field.strip() for field in line.split(",")]
     n_fields = len(fields)
     if n_fields != 4:
-        raise HTTPException(status_code=400, detail=f'Should be 4 fields, not {n_fields} ({line})')
+        raise HTTPException(
+            status_code=400, detail=f"Should be 4 fields, not {n_fields} ({line})"
+        )
     try:
-        transaction = Transaction(date=fields[0], category=fields[1], amount=fields[2], memo=fields[3])
+        transaction = Transaction(
+            date=fields[0], category=fields[1], amount=fields[2], memo=fields[3]
+        )
     except Exception as err:
-        raise HTTPException(status_code=400, detail=f'Cannot parse one of the CSV lines. Details: {str(err)}')
+        raise HTTPException(
+            status_code=400,
+            detail=f"Cannot parse one of the CSV lines. Details: {str(err)}",
+        )
     return transaction
-
-   
